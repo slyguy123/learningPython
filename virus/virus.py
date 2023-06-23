e4dc0db2782b621a297030626b950f85c9d8ec09
slyguy123 <guy.schuleman@gmail.com>
2023-06-23 21:02:52 +0100

    Revert "Adding definition"
    This reverts commit 6367e7c23551f0472bb76bd0baf407d3a6253262.

--- virus/virus.py
+++ virus/virus.py
@@ -8,31 +8,27 @@ startingPath = "/home/slyguy/Documents/pythonProjects"
 fileName = "script.py"
 curDir = os.getcwd()
 
-def main():
-    # check if file exists
-    if(os.path.exists(curDir+"/"+fileName)):
-        print(" File " + fileName + " already exists! ")
-        print(" Deleting . . . ")
-        os.remove(curDir + "/" + fileName)
-        print(" File removed! ")
+# check if file exists
+if(os.path.exists(curDir+"/"+fileName)):
+    print(" File " + fileName + " already exists! ")
+    print(" Deleting . . . ")
+    os.remove(curDir + "/" + fileName)
+    print(" File removed! ")
 
-    # if not do all this good stuff
-    else:
-        print( "Building virus . . . ")
-        # write info to a file
-        fileText = "This could be virus code"
-        f= open(fileName, "w+")
-        f.write(fileText)
-        f.close()
+# if not do all this good stuff
+else:
+    print( "Building virus . . . ")
+    # write info to a file
+    fileText = "This could be virus code"
+    f= open(fileName, "w+")
+    f.write(fileText)
+    f.close()
 
-        # loop over directories
-        baseDir = startingPath
-        for folder in os.listdir(baseDir):
-            fodPath = os.path.join(baseDir, folder)
-            #check if directory
-            if (os.path.isfile(fodPath) == False):
-                #copy file to other locations
-                shutil.copy(curDir + "/" + fileName, curDir + "/" +folder)
-
-
-main()
+    # loop over directories
+    baseDir = startingPath
+    for folder in os.listdir(baseDir):
+        fodPath = os.path.join(baseDir, folder)
+        #check if directory
+        if (os.path.isfile(fodPath) == False):
+            #copy file to other locations
+            shutil.copy(curDir + "/" + fileName, curDir + "/" +folder)
